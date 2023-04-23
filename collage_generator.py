e12f2d057b06467fe42fcd6991d4ace0e50bec1e
Ashwin Hingwe <ashwinh@utexas.edu>
2023-04-22 23:36:30 -0500

    Downloaded images and created collage of collages to stay within image dimension limit

--- collage_generator.py
+++ collage_generator.py
@@ -34,62 +34,83 @@ for link in a_tags:
     img_urls.append(img_url)
     labels.append(label)
 
-    response = requests.get(img_url)
-    img = Image.open(io.BytesIO(response.content))
-    img.save('images/' + labels[counter] + '.jpg')
-    counter += 1
+    # response = requests.get(img_url)
+    # img = Image.open(io.BytesIO(response.content))
+    # if img.mode != 'RGB':
+    #     img = img.convert('RGB')
+    # img.save('images/' + labels[counter] + '.jpg')
+    # counter += 1
 
 print(f'Extracted image links: {len(img_urls)}')
 
-# images = []
-# counter = 0
-# for url in img_urls:
-#     response = requests.get(url)
-#     img = Image.open(io.BytesIO(response.content))
-#     img.thumbnail((1000,1000))
-#     images.append(img)
-#     print(f'image opened: {counter}')
-#     counter += 1
 
-# print(f'Extracted image data: {len(images)}')
+counter2 = 0
+curr = 0
+for i in range(210//5, 210 + 210//5, 210//5):
+    images = []
+    counter = 0
+    for url in img_urls[curr:i]:
+        img = Image.open('images/' + labels[counter2] +'.jpg')
+        img.thumbnail((1000,1000))
+        images.append(img)
+        print(f'image opened: {counter2}')
+        counter2 += 1
 
-# # Calculate the width and height of the collage based on the number of images
-# width = sum(img.width for img in images)
-# height = max(img.height for img in images)
-# print(f'Calculated collage dims: {width}, {height}')
-# # # Create a new blank image for the collage
-# unlabeled_collage = Image.new('RGB', (width, height), (255, 255, 255))
-# print('Made blank image')
-# # Paste each image into the collage
-# counter = 0
-# x_offset = 0
-# for img in images:
-#     unlabeled_collage.paste(img, (x_offset, 0))
-#     print(f'Collaged {labels[counter]}')
-#     #collage.text((x_offset,height-20), labels[counter], fill=(0,0,0))
-#     x_offset += img.width
-#     counter += 1
+    print(f'Extracted image data: {len(images)}')
 
+    # Calculate the width and height of the collage based on the number of images
+    width = sum(img.width for img in images)
+    height = max(img.height for img in images)
+    print(f'Calculated collage dims: {width}, {height}')
+    # # Create a new blank image for the collage
+    unlabeled_collage = Image.new('RGB', (width, height), (255, 255, 255))
+    print('Made blank image')
+    # Paste each image into the collage
+    counter = 0
+    x_offset = 0
 
-# # Save the collage as a file
-# unlabeled_collage.save('unlabeled_collage.jpg')
-# Image.MAX_IMAGE_PIXELS = None
-# collage = Image.open('unlabeled_collage.jpg')
-# canvas = ImageDraw.Draw(collage)
-
-# fontSize = 30
-# myFont = ImageFont.truetype('arial.ttf', fontSize)
-# counter = 0
-# x_offset = 0
-# for img in images:
-#     print(f'Labeled {labels[counter]}')
-#     canvas.text((x_offset+20, height-(fontSize+20)), labels[counter], font = myFont, fill=(0,0,0))
-#     x_offset += img.width
-#     counter += 1
-    
-# collage.show()
-# collage.save('collage.jpg')
+    for img in images:
+        unlabeled_collage.paste(img, (x_offset, 0))
+        #print(f'Collaged {labels[counter]}')
+        #collage.text((x_offset,height-20), labels[counter], fill=(0,0,0))
+        x_offset += img.width
+        counter += 1
 
 
+    # Save the collage as a file
+    unlabeled_collage.save('unlabeled_collage.jpg')
+    Image.MAX_IMAGE_PIXELS = None
+    collage = Image.open('unlabeled_collage.jpg')
+    canvas = ImageDraw.Draw(collage)
 
+    fontSize = 30
+    myFont = ImageFont.truetype('arial.ttf', fontSize)
+    counter = curr
+    x_offset = 0
+    for img in images:
+        print(f'Labeled {labels[counter]}')
+        canvas.text((x_offset+20, height-(fontSize+20)), labels[counter], font = myFont, fill=(0,0,0))
+        x_offset += img.width
+        counter += 1
+        
+    #collage.show()
+    collage.save(f'collage{i//42}.jpg')
+    print(f'Collaged image {i//42}')
+    curr = i
 
+collages = []
+for i in range(5):
+    collages.append(Image.open(f'collage{i+1}.jpg'))
+
+global_width = max(img.width for img in collages)
+global_height = sum(img.height for img in collages)
+
+full_collage = Image.new('RGB', (global_width,global_height), (255,255,255))
+y_offset = 0
+
+for img in collages:
+    full_collage.paste(img, (0,y_offset))
+    y_offset += img.height
+
+full_collage.save('full_collage.jpg')
+full_collage.show()
